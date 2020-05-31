00bb0b5465a80979ed842a76f22c27f929468f73
anthony zhang <anthony.zhang@epita.fr>
2020-05-31 14:31:03 +0200

    [ADD][MAIN] primitive parser

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,30 @@ import scala.concurrent.{Await, Future, Promise}
 import scala.swing
 import scala.swing.{Frame, SimpleSwingApplication}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.StdIn.readLine
 
 object Main {
     def main(args: Array[String]): Unit = {
         val (air, count, run) = CsvParser.parse(Array("airports.csv", "countries.csv", "runways.csv"))
         MyDB.populate(air, count, run)
+
+        println("You can get a Report by typing Report")
+        println("You can get information on a specific airport" +
+          " by typing Query followed by airport name or code")
+
+        val action = readLine()
+
+        if (action.contains(" "))
+        {
+            val query = action.split(" ")
+            if (query(0).contentEquals("Query"))
+            {
+                println("Call to Query with argument")
+            }
+        }
+        else if (action.contentEquals("Report"))
+        {
+            println("Call to Report")
+        }
     }
 }
